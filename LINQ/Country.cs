0781a821e0cc4378f8616bda6d00752e419a326f
TUP <turcsanyipatrik@gmail.com>
2022-11-18 17:38:18 +0100

    Add project files.

---
+++ LINQ/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class Country
+    {
+        //public static int id { get; internal set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ LINQ/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQ
+{
+    public partial class Form1 : Form
+    {
+        List<Country> countries = new List<Country>();
+        List<Ramen> ramens = new List<Ramen>();
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData("ramen.csv");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void LoadData(string filename)
+        {
+            using (StreamReader sr = new StreamReader(filename, Encoding.Default))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+
+                    var countryName = line[2];
+                    Addcountry(countryName);
+
+                    var ramen = new Ramen()
+                    {
+                        ID = ramens.Count + 1,
+                        Brand = line[0],
+                        Name = line[1],
+                        Country = Country,
+                        CountryFK = Country.ID,
+                        Stars = Convert.ToDouble(line[3])
+                    };
+                    ramens.Add(ramen);
+
+                    /* var currentCountry = (from c in countries where c.Name.Equals(countryName) select c).FirstOrDefault();
+
+                     var ramen = new Ramen()
+                     {
+                         ID = ramens.Count + 1,
+                         Brand = line[0],
+                         Name = line[1],
+                         Country = Country,
+                         CountryFK = Country.ID,
+                         Stars = Convert.ToDouble(line[3])
+                     };
+                     ramens.Add(ramen);
+
+                     if (currentCountry == null)
+                     {
+                         currentCountry = new Country()
+                         { 
+                             ID = countries.Count + 1,
+                             Name = countryName
+                         };
+                         countries.Add(currentCountry);
+                     }*/
+
+                }
+            }
+        }
+
+        public Country Addcountry(string countryName)
+        {
+            var currentCountry = (from c in countries where c.Name.Contains(countryName) select c).FirstOrDefault();
+
+            if (currentCountry == null)
+            {
+                currentCountry = new Country()
+                {
+                    ID= countries.Count + 1,
+                    Name= countryName
+                };
+                countries.Add(currentCountry);
+            }
+            
+            return currentCountry;
+        }
+    }
+}
